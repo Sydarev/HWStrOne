1133eede7d37424f1e39d9e1c2cebac6e343df28
Sudarev <soul7341@gmail.com>
2023-10-23 15:19:02 +0300

    Created Main and StreamMain

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Integer> list = Arrays.asList(1, 2, 5, 16, -1, -2, 0, 32, 3, 5, 8, 23, 4);
+        List<Integer> result = new ArrayList<>();
+        for (int i = 0; i < list.size(); i++) if (list.get(i) > 0 && list.get(i) % 2 == 0) result.add(list.get(i));
+
+        int temp;
+        for (int i = 0; i < result.size() - 1; i++) {
+            for (int j = 0; j < result.size() - i - 1; j++) {
+                if (result.get(j) > result.get(j + 1)) {
+                    temp = result.get(j);
+                    result.set(j, result.get(j + 1));
+                    result.set(j + 1, temp);
+                }
+            }
+        }
+        System.out.println(result);
+    }
+}
